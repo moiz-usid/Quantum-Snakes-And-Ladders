e33cbe4fabb843f16c05f7cf36f7f0743d98fb58
HamnaAhsann <124249116+HamnaAhsann@users.noreply.github.com>
2025-05-09 20:34:01 +0500

    Update board.py

--- src/game/board.py
+++ src/game/board.py
@@ -1,1 +1,24 @@
+from utils.config import Config
 
+class Board:
+    def __init__(self, config: Config):
+        self.size = config.board_size
+        self.snakes = {16: 6, 47: 26, 49: 11}  # Head: Tail
+        self.ladders = {3: 38, 8: 28, 20: 42}  # Bottom: Top
+        self.powerup_positions = [10, 30, 50]  # Positions with power-ups
+    
+    def is_snake(self, position):
+        return position in self.snakes
+    
+    def is_ladder(self, position):
+        return position in self.ladders
+    
+    def has_powerup(self, position):
+        return position in self.powerup_positions
+    
+    def get_new_position(self, position):
+        if self.is_snake(position):
+            return self.snakes[position]
+        elif self.is_ladder(position):
+            return self.ladders[position]
+        return position
