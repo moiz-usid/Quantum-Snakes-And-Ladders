234d74338b75b13f67a4b12c9eda795b8a1081f3
HamnaAhsann <124249116+HamnaAhsann@users.noreply.github.com>
2025-05-09 20:34:32 +0500

    Update player.py

--- src/game/player.py
+++ src/game/player.py
@@ -1,1 +1,8 @@
-
+class Player:
+    def __init__(self, id, name, is_ai=False):
+        self.id = id
+        self.name = name
+        self.is_ai = is_ai
+        self.position = 1
+        self.powerups = []  # List of power-up names
+        self.moves = 0  # Track total moves
